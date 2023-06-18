02872800a4ac35121d8a3e1a698ec199fe8df1de
PIV-y <iv.proshin@yandex.ru>
2023-06-18 18:42:37 +0500

    2023.06.18_com1

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,27 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.sql.Time;
 
 @Component
 public class AnimalsCage {
-
+    private Timer time = new Timer();
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+    @Autowired
+    public Timer getTimer() {
+        return time;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(getTimer().getTime());
         System.out.println("________________________");
     }
 }
